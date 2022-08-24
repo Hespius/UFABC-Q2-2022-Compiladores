255e55cf5d78b630fa6bbe1cbda9ff1a3ed81f88
jungoncalves <juliana.neves@aluno.ufabc.edu.br>
2022-08-23 21:30:59 -0300

    add command enquanto

---
+++ IsiLanguage_New/src/br/com/professorisidro/isilanguage/ast/CommandEnquanto.java
@@ -0,0 +1,36 @@
+package br.com.professorisidro.isilanguage.ast;
+
+import java.util.ArrayList;
+
+public class CommandEnquanto extends AbstractCommand {
+
+    private String condition;
+    private ArrayList<AbstractCommand> enquantoCommands;
+
+    public CommandEnquanto(String condition, ArrayList<AbstractCommand> enquantoCommands) {
+        this.condition = condition;
+        this.enquantoCommands = enquantoCommands;
+    }
+
+    @Override
+    public String generateJavaCode() {
+        StringBuilder str = new StringBuilder();
+        str.append("\twhile(").append(condition).append("){\n\t");
+        for(AbstractCommand cmd: this.enquantoCommands){
+            str.append(cmd.generateJavaCode());
+        }
+        str.append("\n\t}");
+        return str.toString();
+    }
+
+    @Override
+    public String toString() {
+    	StringBuilder bdr = new StringBuilder();
+    	for(AbstractCommand c: enquantoCommands)
+    	{
+    		bdr.append("\t");
+    		bdr.append(c);
+       	}
+        return "Comando de repetição lido com sucesso! [enquanto (" + condition + " ) {\n" +  bdr + "\t}" ;
+}
+}
