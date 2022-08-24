7ee0e752ebf28f9dc6b8618a6c07ae33d0b88372
jungoncalves <juliana.neves@aluno.ufabc.edu.br>
2022-08-23 21:57:22 -0300

    deletanto arquivos não utilizados

--- IsiLanguage_New/MainClass.java
+++ IsiLanguage_New/MainClass.java
@@ -4,12 +4,17 @@ public class MainClass{
        Scanner _key = new Scanner(System.in);
 double  a;
 double  b;
+double  c;
 String  t1;
 a= _key.nextDouble();
 b= _key.nextDouble();
 a = 1+2*3/b;
+c = 10;
 if (a>b) {
 System.out.println(a);}else {
 System.out.println(b);}
 
+	while(c<12){
+	System.out.println(c);c = c+1;
+	}
   }}
--- src/br/com/compilador/compiler/exceptions/LexicalException.java
+++
@@ -1,8 +0,0 @@
-package br.com.compilador.compiler.exceptions;
-
-public class LexicalException extends RuntimeException {
-	public LexicalException(String msg) {
-		super(msg);
-	}
-
-}
